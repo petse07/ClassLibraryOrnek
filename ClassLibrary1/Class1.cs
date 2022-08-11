5ac63aefe0f93cfb3c454ee8678d6930946fe057
petse07 <gozcucf@gmail.com>
2022-08-11 13:29:22 +0300

    Proje dosyası ekle.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            int sonuc = 0;
+            sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+    }
+}
